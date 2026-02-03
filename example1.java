a88c6465f24a3647888140b0fcc5a1335b8e258c
PRITIKA.V <pritikavelmurugan9@gmail.com>
2026-02-03 17:42:35 +0530

    Add files via upload

---
+++ example1.java
@@ -0,0 +1,11 @@
+public class example1{
+   public static void main(String args[]){
+     System.out.println("welcome to java");
+     int a=7;
+     int b=8;
+     int c;
+     c=a+b;
+     System.out.println("the sum of a and b is:"+c);
+  }
+}
+    
